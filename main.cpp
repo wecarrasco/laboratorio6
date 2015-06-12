c31d9bc895d8cc4328eff58d7859a18b25396342
Daniela Osorto <odilethpadilla@unitec.edu>
2015-06-11 22:37:48 -0600

    modificacion del main

--- main.cpp
+++ main.cpp
@@ -158,7 +158,7 @@ int main(int argc, char * argv){
 			}
 		}
 		if(opcion == 3){
-			for(int i = 0; i < 	listaAviones.size()-1; i++){
+			for(int i = 0; i < 	listaAviones.size(); i++){
 				cout << listaAviones[i].toString << endl;
 			}
 		}
